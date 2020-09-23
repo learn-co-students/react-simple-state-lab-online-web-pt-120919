75ee7774129f1eacff3b5c8fb57ca0df4dbbe320
seung_kim <seunglives@hotmail.com>
2020-09-23 14:48:07 -0700

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
+import Cell from './Cell.js'
 
 export default class Matrix extends Component {
   
+  //Afer the 
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    console.log(vals)
+    //After genRow() is called through genMatrix, genRow takes a vals argument
+    //the vals argument is basically each individual array of pattern1
+    // When genRow is invoked, it maps through the array and create a Cell component of each color value
+    return vals.map(val => <Cell value={val} />)
   }
   
   genMatrix = () => {
+    // this.props is referencing the pattern1 array as an object: {values: pattern1}
+    //the below code is mapping through the 10 different arrays of pattern1 (this.props.values) and
+    //creating a div to invoke the genRow function/method 
+    
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
   
   render() {
     return (
       <div id="matrix">
-        {this.genMatrix()}
+        {this.genMatrix()} 
+        {/** Through index.js, this is being rendered to the DOM. 
+          This is then rendered. When this is rendered to the DOM, the genMatrix() method/function is invoked
+         */}
       </div>
     )
   }
   
+}
+
+Matrix.defaultProps = {
+  values: (() => {
+    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+    return (new Array(10).fill(defRow))
+  })()
 }
